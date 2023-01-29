d78e1a7f5ae390205f89318a4bd6e7d0b7e07454
Akshat Singh Thakur <akshatthakur7424@gmail.com>
2023-01-29 22:07:27 +0530

    Indian Army Website
    This website is about Indian Army made under a hackathon held by ACM group of Jabalpur Engineering Collage

---
+++ Javascript/filter.js
@@ -0,0 +1,24 @@
+function airForce(){
+    alert("Refresh the page after applying filter to see all the Projects again");
+    document.getElementById("armyUri").style.display = "none";
+    document.getElementById("armyTaj").style.display = "none";
+    document.getElementById("army5140").style.display = "none";
+    document.getElementById("armyGazhi").style.display = "none";
+    document.getElementById("achievements").style.height = "900px";
+}
+
+function navy(){
+    alert("Refresh the page after applying filter to see all the Projects again");
+    document.getElementById("armyUri").style.display = "none";
+    document.getElementById("armyTaj").style.display = "none";
+    document.getElementById("army5140").style.display = "none";
+    document.getElementById("armyBhuj").style.display = "none";
+    document.getElementById("achievements").style.height = "900px";
+}
+
+function IndianArmy(){
+    alert("Refresh the page after applying filter to see all the Projects again");
+    document.getElementById("armyBhuj").style.display = "none";
+    document.getElementById("armyGazhi").style.display = "none";
+    document.getElementById("achievements").style.height = "1700px";
+}
---
+++ readme file.txt
@@ -0,0 +1,3 @@
+Hello this is a project on Indian Army .
+It is created for a online hackathon held by ACM group of Jabalpur Engineering Collage.
+It was an overall nice journey which taught me many things through challenges.
